a51d967b5c5af0b984ada44822c79157d03e7649
Mateusz Głowania <mateusz.glowania@gmail.com>
2022-04-06 23:33:12 +0200

    fix title

--- src/components/atoms/HeroTitle/TitleStyle.js
+++ src/components/atoms/HeroTitle/TitleStyle.js
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const H1Style = styled.h1`
   color: ${(props) => props.theme.colors.secondText};
-  font-size: min(6.4rem, 42px);
+  font-size: min(6.4rem, 15vw);
 `;
 
 const H2Style = styled.h2`
   color: ${(props) => props.theme.colors.secondText};
-  font-size: 3.6rem;
+  font-size: min(4.8rem, 9vw);
+  font-weight: normal;
 `;
 
 export { H2Style, H1Style };
