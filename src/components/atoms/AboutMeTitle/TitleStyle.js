80da37f2d427b9ffb1f4593c1de7958d3f8a6a45
Mateusz Głowania <mateusz.glowania@gmail.com>
2022-04-07 14:31:49 +0200

    add border in TitleAboutMe

--- src/components/atoms/AboutMeTitle/TitleStyle.js
+++ src/components/atoms/AboutMeTitle/TitleStyle.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 const TitleAboutMe = styled.h1`
   font-size: min(7.2rem, 15vw);
   width: 100%;
+  text-align: center;
+  border-bottom: 1rem solid ${(props) => props.theme.colors.primary};
+  margin-bottom: 1.6rem;
 `;
 
 export default TitleAboutMe;
